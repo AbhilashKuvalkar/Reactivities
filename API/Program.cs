29fb6574ba1050c138de028a9ef59de6a355cb8f
Abhilash Kuvalkar <abhilashkuvalkar@live.com>
2025-09-15 23:48:02 +0530

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<AppDbContext>();
+            await context.Database.MigrateAsync();
+            await DbInitializer.SeedDataAsync(context);
+        }
+        catch (Exception exception)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(exception, "An error occurred during migration");
+        }
+
+        await app.RunAsync();
+    }
+}
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext : DbContext
+{
+    public required DbSet<Activity> Activities { get; set; }
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+    
+}
